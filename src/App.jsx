d516264f5232fab730aa48b724df5f5774f90688
Kamie Short <kamie.short@gmail.com>
2022-04-26 10:55:17 -0700

    working through problem set

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { asyncGetQuotes, getQoutes} from "./services/promise-me";
-
+import {
+  asyncGetQuotes,
+  getQoutes,
+  asyncTryGetQuotes,
+  asyncThenTryGetQuotes,
+} from './services/promise-me';
 
 export default function App() {
-const handleClick = (callback) => {
-  const result = callback();
-  console.log(result);
-}
+  const handleClick = (callback) => {
+    const result = callback();
+    console.log(result);
+  };
 
+  return (
+    <>
+      <button onClick={() => handleClick(asyncGetQuotes)}>
+        Async Get Quotes
+      </button>
+      <button onClick={() => handleClick(getQoutes)}>Get Quotes</button>
 
-  return <>
-  <button onClick={() => handleClick(asyncGetQuotes)}>Async Get Quotes</button>
-<button onClick={()=> handleClick(getQoutes)}>Get Quotes
-  </button>
-  </>;
+      <button onClick={async () => console.log(await asyncTryGetQuotes())}>
+        {' '}
+        asyncTryGetQuotes
+      </button>
+      <button onClick={async () => console.log(await asyncThenTryGetQuotes())}>
+        {' '}
+        asyncThenTryGetQuotes
+      </button>
+    </>
+  );
 }
